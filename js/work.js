339ef09c1c0fe746878b98b295985a118f52d8f6
luisfmg14 <mluisfarid@gmail.com>
2019-06-17 12:15:48 -0500

    ok

---
+++ js/work.js
@@ -0,0 +1,9 @@
+var t = document.querySelectorAll(".texto");
+var n = document.querySelectorAll(".nav1");
+for (x=0;x<t.length;x++){
+
+	t[x].innerHTML="Con Fintra encuentra la mejor solución y alternativa para financiar tus estudios. En Fintra te prestamos para tus estudios porque creemos en el poder de tus sueños. Legalización a Domicilio.";
+}
+console.log(n);
+
+n.style.background="blue";
