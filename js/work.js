cb6068520550ce6ccd121dc69debca9a3977fdda
luisfmg14 <mluisfarid@gmail.com>
2019-06-19 10:26:21 -0500

    color

--- js/work.js
+++ js/work.js
@@ -1,9 +1,25 @@
 var t = document.querySelectorAll(".texto");
-var n = document.querySelectorAll(".nav1");
+var n = document.getElementById('box-nav');
+var m = document.getElementById('box-nav2');
+var n1 = document.getElementById('col1');
+var n2 = document.getElementById('col2');
+var n3 = document.getElementById('col3');
+var n4 = document.getElementById('col4');
+var n5 = document.getElementById('col5');
+var n6 = document.getElementById('col6');
 for (x=0;x<t.length;x++){
 
 	t[x].innerHTML="Con Fintra encuentra la mejor solución y alternativa para financiar tus estudios. En Fintra te prestamos para tus estudios porque creemos en el poder de tus sueños. Legalización a Domicilio.";
 }
 console.log(n);
 
-n.style.background="blue";
+function cambiar() {
+ n.style.background="#30B354";
+ m.style.background="#30B354";
+ n1.style.background="#30B354";
+ n2.style.background="#30B354";
+ n3.style.background="#30B354";
+ n4.style.background="#30B354";
+ n5.style.background="#30B354";
+ n6.style.background="#30B354";
+}
